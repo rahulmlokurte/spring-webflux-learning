0a83a64a7c6c5402f2e8ee47330a7c652a17ffc9
Rahul Lokurte <rahul.m.lokurte@gmail.com>
2021-04-26 00:31:36 +0530

    Initial commit

---
+++ src/main/java/com/rahullokurte/springwebfluxlearning/SpringWebfluxLearningApplication.java
@@ -0,0 +1,13 @@
+package com.rahullokurte.springwebfluxlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rahullokurte/springwebfluxlearning/SpringWebfluxLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahullokurte.springwebfluxlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
